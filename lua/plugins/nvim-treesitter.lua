1a46e3f71d9fb4231d563cf2654a7919b8ca336f
Alfredo Mungo <alfredo.mungo@protonmail.ch>
2025-06-24 23:43:26 +0200

    Updated plugin configuration to remove warning

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
 	{
         "nvim-treesitter/nvim-treesitter", branch = 'master',
         lazy = false, build = ":TSUpdate",
-        config = {
+        opts = {
             ensure_installed = { "lua", "c", "python" },
             auto_install = true,
             highlight = { enable = true },
